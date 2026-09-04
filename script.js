5ff56d996fb52e79789b042259bf07af2adfdea9
renan <renan95tito@gmail.com>
2026-09-04 14:09:40 -0300

    Capturei os dados do form com JS

---
+++ script.js
@@ -0,0 +1,11 @@
+const formulario = document.getElementById('meuFormulario')
+
+formulario.addEventListener("submit", function(evento) {
+    evento.preventDefault();
+
+    const dados = new FormData(formulario);
+
+    const peso = dados.get('peso');
+    const objetivo = dados.get('objetivo');
+    const nivel_atividade = dados.get('nivel_atividade');
+});
